2c6fc9277b7a16f283ac6e5494c429eb351ba1f8
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-01-21 23:46:14 +0530

    Create SearchingKey.c

---
+++ SearchingKey.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void ls(int arr[], int n, int key){
+
+int i;
+for(i=0;i<n;i++){
+    if(key==arr[i]){
+        printf("Key found at index : %d " ,i);
+       
+    }  
+ }
+
+}
+
+void main(){
+
+int n=10;
+int arr[]={1,4,6,2,7,3,8,4,9,10};
+int key=6;
+ls(arr,n,key);
+
+}
